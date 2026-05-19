0055a09ccb20dd1646503d4e720d1bd873579858
Kleber Germano <klebergermanodev@gmail.com>
2026-05-19 20:59:55 -0300

    Nav mobile configurado

--- frontend/src/components/navSidebar/NavSidebar.jsx
+++ frontend/src/components/navSidebar/NavSidebar.jsx
@@ -1,11 +1,11 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
-// import { FaShoppingBasket } from "react-icons/fa";
+
 import { BsFillClipboardPlusFill } from "react-icons/bs";
+import { GiHamburgerMenu } from "react-icons/gi";
+
 import {
-    FaShoppingBasket,
     FaHome,
-    FaBoxOpen,
-    FaPlus,
     FaUsers,
     FaUserPlus,
     FaClipboardList
@@ -15,135 +15,155 @@ import "./nav-sidebar.scss";
 
 function NavSidebar() {
 
+    const [menuOpen, setMenuOpen] = useState(false);
+
     const user = JSON.parse(
         localStorage.getItem("user")
     );
 
+    function toggleMenu() {
+        setMenuOpen(!menuOpen);
+    }
+
     return (
 
-        <aside id="nav-sidebar">
+        <aside
+            id="nav-sidebar"
+            className={menuOpen ? "open" : ""}
+        >
 
-            <div className="user-info">
+            <button
+                className="btn-hamburger"
+                onClick={toggleMenu}
+            >
+                <GiHamburgerMenu />
+            </button>
 
-                <img
-                    src={
-                        user?.img_url ||
-                        "https://placehold.co/80x80"
-                    }
-                    alt="Usuário"
-                />
+            <div className="nav-content">
 
-                <h3>
-                    {user?.nome || "Usuário"}
-                </h3>
+                <div className="user-info">
 
-                <span>
-                    {user?.cargo || "Sem cargo"}
-                </span>
+                    <img
+                        src={
+                            user?.img_url ||
+                            "https://placehold.co/80x80"
+                        }
+                        alt="Usuário"
+                    />
+
+                    <h3>
+                        {user?.nome || "Usuário"}
+                    </h3>
+
+                    <span>
+                        {user?.cargo || "Sem cargo"}
+                    </span>
+
+                </div>
+
+                <nav id='nav-lateral'>
+
+                    <ul>
+
+                        <li>
+
+                            <NavLink to="/">
+
+                                <FaHome />
+
+                                <span>
+                                    Início
+                                </span>
+
+                            </NavLink>
+
+                        </li>
+
+                        <li className="menu-group">
+
+                            <p className="nav-section">
+                                Produtos
+                            </p>
+
+                            <ul className="sub-menu">
+
+                                <li>
+
+                                    <NavLink to='/produtos/view-produtos'>
+
+                                        <FaClipboardList />
+
+                                        <span>
+                                            Listar
+                                        </span>
+
+                                    </NavLink>
+
+                                </li>
+
+                                <li>
+
+                                    <NavLink to='/produtos/add-produto'>
+
+                                        <BsFillClipboardPlusFill />
+
+                                        <span>
+                                            Cadastrar
+                                        </span>
+
+                                    </NavLink>
+
+                                </li>
+
+                            </ul>
+
+                        </li>
+
+                        <li className="menu-group">
+
+                            <p className="nav-section">
+                                Usuários
+                            </p>
+
+                            <ul className="sub-menu">
+
+                                <li>
+
+                                    <NavLink to="/users/view">
+
+                                        <FaUsers />
+
+                                        <span>
+                                            Listar
+                                        </span>
+
+                                    </NavLink>
+
+                                </li>
+
+                                <li>
+
+                                    <NavLink to="/users/add">
+
+                                        <FaUserPlus />
+
+                                        <span>
+                                            Cadastrar
+                                        </span>
+
+                                    </NavLink>
+
+                                </li>
+
+                            </ul>
+
+                        </li>
+
+                    </ul>
+
+                </nav>
 
             </div>
-
-            <nav id='nav-lateral'>
-
-                <ul>
-
-                    <li>
-
-                        <NavLink to="/">
-
-                            <FaHome />
-
-                            <span>
-                                Início
-                            </span>
-
-                        </NavLink>
-
-                    </li>
-
-                    <li className="menu-group">
-
-                        <p className="nav-section">
-                            Produtos
-                        </p>
-
-                        <ul className="sub-menu">
-
-                            <li>
-
-                                <NavLink to='/produtos/view-produtos'>
-
-                                    <FaClipboardList />
-
-                                    <span>
-                                        Listar
-                                    </span>
-
-                                </NavLink>
-
-                            </li>
-
-                            <li>
-
-                                <NavLink to='/produtos/add-produto'>
-                                    {/* <FaPlus /> */}
-                                    <BsFillClipboardPlusFill />
-                                    <span>
-                                        Cadastrar
-                                    </span>
-
-
-                                </NavLink>
-
-                            </li>
-
-                        </ul>
-
-                    </li>
-
-                    <li className="menu-group">
-
-                        <p className="nav-section">
-                            Usuários
-                        </p>
-
-                        <ul className="sub-menu">
-
-                            <li>
-
-                                <NavLink to="/users/view">
-
-                                    <FaUsers />
-
-                                    <span>
-                                        Listar
-                                    </span>
-
-                                </NavLink>
-
-                            </li>
-
-                            <li>
-
-                                <NavLink to="/users/add">
-
-                                    <FaUserPlus />
-
-                                    <span>
-                                        Cadastrar
-                                    </span>
-
-                                </NavLink>
-
-                            </li>
-
-                        </ul>
-
-                    </li>
-
-                </ul>
-
-            </nav>
 
         </aside>
 
